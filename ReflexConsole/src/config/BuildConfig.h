af6482bf385d230903d164b24c89ebd1f4f4de98
GrumpyBud <109627078+GrumpyBud@users.noreply.github.com>
2026-06-24 10:09:27 -0400

    chore(config): set touch-only firmware version 1.1.3

--- ReflexConsole/src/config/BuildConfig.h
+++ ReflexConsole/src/config/BuildConfig.h
@@ -1,11 +1,11 @@
 #pragma once
 
-#define FIRMWARE_VERSION "1.0.0"
+// User-visible version shown on the boot screen. Bump it for each release.
+#define FIRMWARE_VERSION "1.1.3"
 #define DEBUG_SERIAL 1
 #define ENABLE_BUZZER 0
 #define ENABLE_WIFI_DASHBOARD 0
 #define USE_TOUCH_INPUT 1
-#define USE_JOYSTICK_INPUT 1
 
 #if DEBUG_SERIAL
   #define DEBUGF(...) Serial.printf(__VA_ARGS__)
